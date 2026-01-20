0288ec047b409d5be47c553507fbb30c3f6233e2
Francois Poizat <francois.poizat@gmail.com>
2026-01-20 18:49:37 +0100

    preparing to publish

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     file_name = sys.argv[1] if len(sys.argv) > 1 else "spec.yaml"
     spec_file = file_name
     project_spec = load_spec_file(spec_file)
-    asyncio.run(process_project(project_spec, workdir=Path("./test_dir"), concurrency=16))
+    asyncio.run(process_project(project_spec, workdir=Path("."), concurrency=16))
