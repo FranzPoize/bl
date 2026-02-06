d6ee6d60b350f4386d2623d7b5ba13693b30717a
Francois Poizat <francois.poizat@gmail.com>
2026-02-06 17:41:59 +0100

    fix bench

--- benchmark/bl_benchmark/bench_cold.sh
+++ benchmark/bl_benchmark/bench_cold.sh
@@ -1,6 +1,6 @@
 hyperfine \
   --prepare 'rm -rf external-src/; rm -rf src' \
   -m 2 \
-  'python -m bl -c noukies-spec.yaml -z noukies-frz.yaml' \
-  'python -m bl -c abilis-spec.yaml' \
-  'python -m bl -c prodotti-spec.yaml'
+  'python -m bl build -c noukies-spec.yaml -z noukies-frz.yaml' \
+  'python -m bl build -c abilis-spec.yaml' \
+  'python -m bl build -c prodotti-spec.yaml'
