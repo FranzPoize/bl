64809b0575be88e18ab207010a3d2b70d911a1ac
Francois Poizat <francois.poizat@gmail.com>
2026-03-24 08:42:33 +0100

    fix git error code printing in debug log

--- bl/utils.py
+++ bl/utils.py
@@ -55,7 +55,7 @@ async def run(*args: str, cwd: Optional[Path] = None) -> tuple[int, str, str]:
     stdout, stderr = await proc.communicate()
     returncode = proc.returncode if proc.returncode is not None else -1
     logger.debug(f"In {str(cwd)}: {' '.join([str(a) for a in args])}")
-    logger.debug(f"{returncode} - {stdout.decode().strip()} - {stdout.decode().strip()}")
+    logger.debug(f"{returncode} - {stdout.decode().strip()} - {stderr.decode().strip()}")
     return returncode, stdout.decode(), stderr.decode()
 
 
